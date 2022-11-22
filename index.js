05cae0b26cbb1753a8b0bfc6fd30fab00210d0f9
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2022-11-22 18:47:16 -0500

    commit tree 2a138977891a3d5bc9a7b43dd5927a0f1d89b624

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers){
+return drivers.slice(0,2);
+}
+
+const returnLastTwoDrivers = function(drivers){
+    return drivers.slice((drivers.length-2),(drivers.length));
+    }
+
+    const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+    function createFareMultiplier(number){
+        return function multiplier(fare){
+            return fare*number;
+        }
+    }
+    const fareDoubler = createFareMultiplier(2);
+
+    const fareTripler = createFareMultiplier(3);
+
+    function selectDifferentDrivers(drivers, selection){
+        if (selection === returnFirstTwoDrivers){
+            return returnFirstTwoDrivers(drivers);
+        }
+        else if(selection === returnLastTwoDrivers){
+            return returnLastTwoDrivers(drivers);
+        }
+    }
